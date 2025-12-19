199f967d9084b06c6c90feb1f8b2568949079719
hshus <hshus087@gmail.com>
2025-12-19 21:15:15 +0300

    справлен baseURL

--- fixture/application.py
+++ fixture/application.py
@@ -30,7 +30,7 @@ class Application:
 
     def open_home_page(self):
         wd = self.wd
-        if not wd.current_url.endswith("/mantisbt-1.2.20/"):
+        if not wd.current_url(self.app.base_url):
             wd.get(self.base_url)
 
     def destroy(self):
